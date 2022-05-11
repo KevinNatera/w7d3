849f87e22ad5922c3508bd2a68d925e99dd0571e
Kevin <NateraKevin1@gmail.com>
2022-05-11 18:58:06 -0400

    created users factory bot file

--- Goal_App/spec/factories/users.rb
+++ Goal_App/spec/factories/users.rb
@@ -1,5 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+  end
+
+  factory :default_user do 
+    email { 'anonymous@gmail.com'}
   end
 end
